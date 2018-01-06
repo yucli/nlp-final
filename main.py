9aa39e75b6f9a4c5e7e040e934c5046cb74bb046
yuclose <yuclose@users.noreply.github.com>
2018-01-06 22:22:01 +0800

    Update basic structure

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 from pkg.model import TermSelection as TS
+from pkg.model import TermOrdering as TO
+from pkg.model import TitleLength as TL
 
 corpus = {
-    'movie': {
-        '哈嗚': ['哈哈哈', '嗚嗚嗚']
+    '電影名': {
+        '復仇者聯盟': {
+            '電影名斷詞': ['復仇者', '聯盟']
+            '電影名斷詞詞性': ['N', 'N']
+            '字': ['我', '打']
+            '字詞性': ['N', 'V']
+            '字數量': ['1000', '50']
+        }
+
     }
 }
+tf_idf_words_ten_test = [[('毀滅', 'V'), ('聯盟', 'N'), ('相逢', 'V')], []] # 10部測試電影, 共3000words
+
 TSModel = TS(corpus)
-TSModel.score()
+TOModel = TO(corpus)
+TLModel = TL(corpus)
+
+words_ten_test = TSModel.gen_words_num_test(tf_idf_words_ten_test, 5)
+generated_terms_of_titles_ten_test = TOModel.gen_terms_titles_num_test(words_ten_test, 200)
+generated_title_ten_test = TLModel.gen_title_num_test(generated_terms_of_titles_ten_test)
+
+print(generated_title_ten_test)
--- pkg/model.py
+++ pkg/model.py
@@ -4,9 +4,10 @@ import math
 class Model():
 	def __init__(self, corpus):
 		self.corpus = dict(corpus)
-		self.movie_num = len(self.corpus['movie'])
-		self.titles = list(self.corpus['movie'].keys()) # list type
-		self.terms_of_titles = [['不可能的', '任務'], ['露西']] # 斷詞後
+		self.movie_num = len(self.corpus['電影名'])
+		self.titles = list(self.corpus['電影名'].keys()) # list type
+		self.terms_of_titles = [self.corpus['電影名'][t]['電影名斷詞'] for t in titles] # 斷詞後
+		self.pos_of_terms_of_titles = [self.corpus['電影名'][t]['電影名斷詞詞性'] for t in titles] # 斷詞後
 
 	
 	def score(self):
@@ -19,48 +20,96 @@ class TermSelection(Model):
 	def score(self):
 		super().score()
 
-	def words(self, length):
-		return ['神鬼', '聯盟']
+	def gen_words_num_test(self, tf_idf_words_num_test, length):
+		return list([[('毀滅', 'V'), ('聯盟', 'N'), ('相逢', 'V')], []])
 
 class TermOrdering(Model):
-	def __init__(self, corpus, words):
+	def __init__(self, corpus):
 		super().__init__(corpus)
-		self.words = words
-		self.pos = {'N': ['飯', '麵'], 'V', ['吃']} # 將words依詞性分類
-
-	def gen_titles(self):
-		generated_titles = []
-		return generated_titles
+		self.train_pos_distribution = {}
+		for pos_of_terms in self.pos_of_terms_of_titles:
+			tuple_pos_of_terms = tuple(pos_of_terms)
+			self.train_pos_distribution[tuple_pos_of_terms] = self.train_pos_distribution.get(tuple_pos_of_terms, 0) + 1
+		for pos_of_terms in self.pos_of_terms_of_titles:
+			tuple_pos_of_terms = tuple(pos_of_terms)
+			self.train_pos_distribution[tuple_pos_of_terms] = self.train_pos_distribution.get(tuple_pos_of_terms) / self.movie_num
+		# 如 train_pos_distribution[('V', 'N')] = 5 / 54
+	def gen_terms_titles_num_test(self, selected_words_num_test, number):
+		generated_terms_of_titles_num_test = []
+		for test in selected_words_num_test:
+			classified_by_pos = {} # 如{'V': ['毀滅', '相逢'], 'N', ['聯盟']} 將words依詞性分類, 且其有相似度的順序
+			generated_terms_of_titles_num_test.append(self.gen_terms_titles(classified_by_pos, number)) # 如 [['毀滅', '聯盟'], ['相逢', '聯盟']]
+		return list(generated_terms_of_titles_num_test)
 
 	def score(self):
 		super().score()
 
-	def gen_n_gram(self):
-		tri_gram = {}
-		trigram['(N, V)'] = 5
+	def gen_terms_titles(self, classified_by_pos, number):
+		terms_titles = []
+		for ordered_pos_s, probability in self.train_pos_distribution.items():
+			needed_count = int(number * probability)
+			terms_titles.extend(gen_needed_terms_titles(classified_by_pos, ordered_pos_s, needed_count))
+			"""
+			temp_terms_titles = []
+			while len(temp_terms_titles) != needed_count:
+				if add_terms_title(temp_terms_titles, ordered_pos_s, classified_by_pos) == False:
+					break
+			terms_titles.extend(temp_terms_titles)
+			"""
+		return terms_titles
+
+	def add_terms_title(self, temp_terms_titles, ordered_pos_s, classified_by_pos):
+		terms_title = []
+		while len(terms_title) != 1: # 或 == 0
+			for pos in ordered_pos_s:
+				if pos not in classified_by_pos.keys():
+					return False
+				terms_title.append(classified_by_pos[pos][])
+			if terms_title in temp_terms_titles:
+				terms_title = []
+			else:
+				temp_terms_titles.append(terms_title)
+		return True
+
+	def gen_needed_terms_titles(self, classified_by_pos, ordered_pos_s, needed_count):
+		generated_needed_terms_titles = []
 		
+		for pos in ordered_pos_s:
+			if pos not in classified_by_pos.keys():
+				return []
+			length_of_each_pos.append(len(classified_by_pos[pos]))
+		
+		generated_needed_terms_titles_length = 1
+		for pos in ordered_pos_s:
+			generated_needed_terms_titles_length *= length_of_each_pos
+
+		generated_needed_terms_titles.extend()
+		return generated_needed_terms_titles[:needed_count]
 
 class TitleLength(Model):
-	def __init__(self, corpus, generated_titles):
+	def __init__(self, corpus):
 		super().__init__(corpus)
-		self.generated_titles = generated_titles
-		self.lenterms_of_titles = [len(t) for t in self.title_terms]
+		self.lenterms_of_titles = [len(t) for t in self.terms_of_titles]
 		self.lenchars_of_titles = [len(t) for t in self.titles]	
 		self.g1 = 1
 		self.g2 = 1
 
-	def gen_title(self):
-		scores = [self.score(t) for t in self.generated_titles]
-		return self.generated_titles[scores.index(max(scores))]
+	def gen_title_num_test(self, generated_terms_of_titles_num_test):
+		gen_titles = []
+		for test in generated_terms_of_titles_num_test:
+			scores = [self.score(terms) for terms in test]
+			gen_titles.append(''.join(test[scores.index(max(scores))]))
+		return gen_titles
 
-	def score(self, title):
+	def score(self, terms):
 		super().score()
-		terms = ['產生', '標題的', '斷', '詞']
+		title = ''.join(terms)
 		lenterms = len(terms)
 		lenchars = len(title)
 		N = self.movie_num
-		P_term = same_lenterms_num(lenterms) / N
-		P_char = same_lenchars_num(lenchars) / N
+		# 加1避免0在log的錯誤
+		P_term = (self.same_lenterms_num(lenterms) + 1 ) / N
+		P_char = (self.same_lenchars_num(lenchars) + 1 ) / N
 
 		return math.log(math.pow(P_term, self.g1) * math.pow(P_char, self.g2), 2)
 	
@@ -77,11 +126,11 @@ class TitleLength(Model):
 			if lenterms == length:
 				num = num + 1
 		return num
-
+"""
 class TitleLength2(Model): # 暫時不用, 尚未完全
 	def __init__(self, corpus, titles):
 		super().__init__(corpus)
-		self.lenterms_of_titles = [len(t) for t in self.title_terms]
+		self.lenterms_of_titles = [len(t) for t in self.terms_of_titles]
 		self.lenchars_of_titles = [len(t) for t in self.titles]
 
 	def get_maxlen_pairs(self):
@@ -106,4 +155,5 @@ class TitleLength2(Model): # 暫時不用, 尚未完全
 				maxcnt_lengths.append(k)
 
 		return maxcnt_lengths
+"""
 

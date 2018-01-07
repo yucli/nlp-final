6fb104d7f52a65c0ef4b9a530b9a5b48e5df0faa
yuclose <yuclose@users.noreply.github.com>
2018-01-07 14:21:32 +0800

    Update corpus structure in main.py

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ corpus = {
     '電影名': {
         '復仇者聯盟': {
             '電影名斷詞': ['復仇者', '聯盟']
+            '電影名斷詞命名實體': [False, False]
             '電影名斷詞詞性': ['N', 'N']
-            '字': ['我', '打']
-            '字詞性': ['N', 'V']
-            '字數量': ['1000', '50']
+            '電影名斷詞詞頻在電影名': ['1', '1']
+            '電影名斷詞詞頻在電影字幕': ['100', '100']
+        },
+        '瑪莉是瑪莉': {
+            '電影名斷詞': ['瑪莉', '是', '瑪莉'] # 在當wj時我會先將重複的濾掉
+            '電影名斷詞命名實體': [True, False, True]
+            '電影名斷詞詞性': ['N', 'V', 'N']
+            '電影名斷詞詞頻在電影名': ['2', '1', '2']
+            '電影名斷詞詞頻在電影字幕': ['100', '1000', '100']
         }
+
 
     }
 }
@@ -21,7 +29,7 @@ TSModel = TS(corpus)
 TOModel = TO(corpus)
 TLModel = TL(corpus)
 
-generated_words_10_test = TSModel.gen_words_num_test(tf_idf_words_10_test, 5)
+generated_words_10_test = TSModel.gen_words_num_test(tf_idf_words_10_test, 100)
 generated_terms_titles_10_test = TOModel.gen_terms_titles_num_test(generated_words_10_test, 200)
 generated_title_10_test = TLModel.gen_title_num_test(generated_terms_titles_10_test)
 
